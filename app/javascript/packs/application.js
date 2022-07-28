a59f515bf11dfd1b75d34b5df857f6f52cee98a6
antimasingh070 <antimasingh3012200@gmail.com>
2022-07-28 20:14:44 +0530

    Add migration file

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,15 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+import "bootstrap"
---
+++ db/schema.rb
@@ -0,0 +1,70 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_140918) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_Of_units"
+    t.text "discription"
+    t.integer "library_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "count"
+    t.index ["library_id"], name: "index_books_on_library_id"
+  end
+
+  create_table "librarians", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_librarians_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_librarians_on_reset_password_token", unique: true
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string "name"
+    t.string "university"
+    t.string "location"
+    t.integer "borrow_limit"
+    t.float "overdue_fines"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password"
+    t.string "university"
+    t.integer "max_copy_of_book_allowed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "books", "libraries"
+end
